b8088667c2f024ed53df1255ca8a656f9c071575
Zarley Vedad <44680477+zarley093@users.noreply.github.com>
2021-03-19 10:54:51 +0800

    Add files via upload

---
+++ pages/books/[id]/[specificBook].tsx
@@ -0,0 +1,56 @@
+const bookSeriesWithId = [
+  {
+    id: 1,
+    name: 'Harry Potter and the Philosopher`s stone',
+    publication_date: '1997'
+  },
+  {
+    id: 2,
+    name: 'Harry Potter and the Chamber of Secrets',
+    publication_date: '1998'
+  },
+  {
+    id: 3,
+    name: 'Harry Potter and the Prisoner of Azkaban',
+    publication_date: '1999'
+  },
+  {
+    id: 4,
+    name: 'Harry Potter and the Goblet of Fire',
+    publication_date: '2000'
+  }
+]
+
+const test = (
+  <div>
+    test
+  </div>
+)
+
+// class RenderBook <{book:any}, {}> {
+//   constructor(props: any) {
+//     super(props);
+//   },
+//   public render () {
+//     <div className='flex flex-row p-2 rounded-lg bg-gray-300 m-1'>
+//       {
+//         book.id 
+//         ? <div className="m-1">
+//             {book.id}
+//           </div> 
+//         : <div className="m-1">
+//           {index} (No Id)
+//         </div>
+//       }
+      
+//       <div className="m-1">
+//         {book.name}
+//       </div>
+//       <div className="m-1">
+//         {book.publication_date}
+//       </div>
+//     </div>
+//   }
+// }
+
+export default test
